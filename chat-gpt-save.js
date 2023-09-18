12a5e1b2c123d8992d8f11b29fc76cf83fa0e583
unqueued <unqueued@gmail.com>
2023-09-17 20:04:49 -0400

    Autogen name, fix file downloading
    File extension is infered by browser by mime-type.
    Generate filename from title of conversation.

--- chat-gpt-save.js
+++ chat-gpt-save.js
@@ -1,11 +1,16 @@
 javascript: (function () {
-    var file_name = prompt("Enter a file name to save the chat:");
-    const currentDate = new Date();
-    file_name = file_name || "chatgpt_" + currentDate.getFullYear() + "-" +
-        (currentDate.getMonth() + 1).toString().padStart(2, "0") + "-" +
-        currentDate.getDate().toString().padStart(2, "0") + "-" +
-        currentDate.getHours().toString().padStart(2, "0") + "-" +
-        currentDate.getMinutes().toString().padStart(2, "0");
+
+
+    const title = document.querySelector('ol li a.bg-gray-800')?.textContent ?? document.title;
+    const non_letters_re = /[^\p{L}\p{N}]+/gu;
+    const trailing_dash_re = /(^-)|(-$)/g;
+    const slug = title.toLowerCase()
+        .replace(non_letters_re, "-")
+        .replace(trailing_dash_re, '');
+
+    var chat_id = window.location.pathname.split('/').pop();
+
+    var file_name = "chatgpt_" + slug + "_" + chat_id + ".html";
 
     const link = document.createElement('a');
     var chat_body = document.querySelector('[class^="flex-1 overflow-hidden"]').innerHTML
@@ -17,6 +22,9 @@ javascript: (function () {
 
         <head>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/default.min.css">
+            <script>
+                myProps=${JSON.stringify(__NEXT_DATA__.props)}
+            </script>
         <style>
             [class^="flex flex-col text-sm"]>div:nth-child(2n+1) {
                 background: lightgray;
@@ -35,6 +43,8 @@ javascript: (function () {
         </head>
 
         <body>
+            <p id="chatid">${chat_id}</p>
+            <p><a id="chatURL" href="${window.location}">${window.location}</a></p>
         ${chat_body}
         </body>
 
@@ -42,7 +52,7 @@ javascript: (function () {
         `
     ],
         { type: 'text/html' }));
-    link.download = file_name + '.html';
+    link.download = file_name;
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
